73615f96c915cd76200e96769cb174b054469020
I3arni <Nik.13aryshev@yandex.ru>
2023-12-10 23:51:18 +0300

    2 commit

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 const buttonElem = document.querySelector('.section__button');
 const modalElem = document.querySelector('.modal');
 
@@ -8,9 +9,34 @@ opacity: 0;
 transition: opacity 300ms ease-in-out;
 `;
 
+const closeModal = event => {
+    const target = event.target;
+
+    if (target === modalElem || target.closest ('.modal__close')) {
+        
+        modalElem.style.opacity = 0;
+
+        setTimeout (() => {
+            modalElem.style.visibility = 'hidden';
+        }, 300)
+    }
+}
+
 const openModal = () => {
     modalElem.style.visibility = 'visible';
     modalElem.style.opacity = 1;
 }
 
 buttonElem.addEventListener('click', openModal);
+modalElem.addEventListener('click', closeModal)
+
+
+$(document).ready(function(){
+    $('.slider').slick({
+        arrows:false,
+        dots:true,
+        fade: true
+    });
+});
+
+
